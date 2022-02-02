e838e9233c92b6ec5a21dde05a479519c3b843d6
Katelyn Lydeen <klydeen@hawk.iit.edu>
2022-02-01 21:11:58 -0600

    Initial Commit

---
+++ Shared/Simple_Series_ProblemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Series_ProblemApp.swift
+//  Shared
+//
+//  Created by Katelyn Lydeen on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Simple_Series_ProblemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
